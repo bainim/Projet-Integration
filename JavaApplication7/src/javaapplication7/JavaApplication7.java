cfccf8056111d135732e5850fa16e6eae63d92b7
marwen4ever <beyni_marwen@yahoo.fr>
2014-02-26 12:24:08 +0100

    test1

---
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication7;
+
+/**
+ *
+ * @author Marwen
+ */
+public class JavaApplication7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int     // TODO code application logic here
+    }
+    
+}
